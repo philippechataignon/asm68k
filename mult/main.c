52d7e52c4b71932fe253e7d4aee8669a1b2debcc
Philippe Chataignon <philippe@ldlc>
2022-05-22 14:46:20 +0200

    Main/mult Ok

--- mult/main.c
+++ mult/main.c
@@ -4,12 +4,13 @@ int asm_multi(int a, int b);
 
 int main()
 {
-    unsigned int a = 132121312L;
-    unsigned int b = 11L;
+    unsigned int a = 12345;
+    unsigned int b = 1234;
     unsigned int r = 0;
     for(int i = 0; i < 100000; i++) {
        r = asm_multi(a, b);
     }
     printf("r=%d\n", r);
+    getchar();
     return 0;
 }
